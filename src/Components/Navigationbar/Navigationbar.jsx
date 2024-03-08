1f2523396a5a4ecbd139d20eb5bdbf517338c952
Brian Churchill Thuo <104701046+Brianthuo@users.noreply.github.com>
2024-03-08 23:16:00 +0300

    Errors resolved

--- src/Components/Navigationbar/Navigationbar.jsx
+++ src/Components/Navigationbar/Navigationbar.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navigationbar.scss'
 import { FiShoppingCart } from "react-icons/fi";
 import { FaRegHeart } from "react-icons/fa";
 import {  useLocation, Link }from 'react-router-dom'
 import { RiMenu3Fill } from "react-icons/ri"; 
+import { IoClose } from "react-icons/io5";
 
 const Navigationbar = () => { 
-
+    const [Open, setOpen] = useState(false)
+    console.log(Open)
     const location = useLocation();
     const cart = JSON.parse(localStorage.getItem('cart'));
     const fav = JSON.parse(localStorage.getItem('Favourites'));
     const itemCount = cart ? (cart.length > 0 ? cart.length : 0) : null;
     const favCount = fav ? (fav.length > 0 ? fav.length : 0) : null;
+
+    const toogleOpen = ()=>{
+      setOpen(prevState => !prevState)
+    }
+    
+    
   return (
-    <div className="Nav">
+    <div className= 'Nav'>
      <div className="Logo">
           <h1>Ecom</h1>
     </div>
-
-    <div className="Ul">
+    <div className={Open ? 'Ulopen' : 'Ul'}>
         <ul>
         <Link  to= '/'className={location.pathname === "/" ? "selected" : "Link"}   >
           <li>Home</li>      
@@ -47,6 +54,14 @@ const Navigationbar = () => {
             </ul>
           </li>
           <li>Our services</li>
+      
+              <Link  to= '/Cart'className={location.pathname === "/Cart" ? "selected" : "Link"}   >
+            <li className={Open ? 'OpenCartAndFav' : 'CartAndFav'} >Cart {itemCount}</li>    
+              </Link>
+              <Link  to= '/Favorites'className={location.pathname === "//Cart" ? "selected" : "Link"}   >
+            <li className={Open ? 'OpenCartAndFav' : 'CartAndFav'}>Favorites {favCount}</li>    
+              </Link>
+          
         </ul>
     </div>
 
@@ -74,7 +89,8 @@ const Navigationbar = () => {
     </div>
 
     <div className="Menu">
-    <RiMenu3Fill className='MenuIcon' />
+      {Open ? <IoClose className='MenuIcon' onClick={toogleOpen} />:  <RiMenu3Fill className='MenuIcon' onClick={toogleOpen} />   }
+     
     </div>
 
        </div>
